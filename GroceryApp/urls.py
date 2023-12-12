a63f7b8ed11e75e1e99e971ecb37d0de8202361f
mariam-daib <mariam.diab@ejust.edu.eg>
2023-12-12 11:50:31 +0200

    Set up Templates cont.

--- GroceryApp/urls.py
+++ GroceryApp/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from GroceryApp.views import index
+from GroceryApp.views import *
 
 app_name = "GroceryApp"
 
 urlpatterns = [
     path("", index),
+    path("checkout", checkout),
+    path("contact", contact),
+    path("shop_details", shop_details),
+    path("shop_grid", shop_grid),
+    path("shopping_cart",shopping_cart),
     ]
--- GroceryApp/views.py
+++ GroceryApp/views.py
@@ -4,3 +4,21 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'GroceryApp/index.html')
+
+def checkout(request):
+    return render(request, 'GroceryApp/checkout.html')
+
+def contact(request):
+    return render(request, 'GroceryApp/contact.html')
+
+def shop_details(request):
+    return render(request, 'GroceryApp/shop-details.html')
+
+def shop_grid(request):
+    return render(request, 'GroceryApp/shop-grid.html')
+
+def shopping_cart(request):
+    return render(request, 'GroceryApp/shoping-cart.html')
+
+
+
