9aad3c4d9be041cd5711801022ec94d5c0974a8e
toqaalaa20 <toqa.alaa@ejust.edu.eg>
2023-12-18 10:17:10 +0200

    Product List in Templates

--- GroceryApp/views.py
+++ GroceryApp/views.py
@@ -1,9 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from GroceryApp.models import Category, Product, ProductImages
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'GroceryApp/index.html')
+    products = Product.objects.all() 
+
+    context = {
+        "products" : products
+    }
+    return render(request, 'GroceryApp/index.html', context)
 
 def checkout(request):
     return render(request, 'GroceryApp/checkout.html')
