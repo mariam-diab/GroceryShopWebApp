aca2aa7435a453b5c24c4ab1ab01f625edbb93f0
mariam-daib <mariam.diab@ejust.edu.eg>
2023-12-21 19:24:14 +0200

    fixing urls

--- GroceryApp/urls.py
+++ GroceryApp/urls.py
@@ -14,4 +14,10 @@ urlpatterns = [
     # path("shop_grid/<str:category>/", shop_grid, name="shop_grid"),
     path("wish_list", wish_list, name= "wish_list"),
     # path("shop_grid/<str:product>/", shop_grid, name="shop_grid_search"),
+    path('update_order/', update_order, name='update_order'),
+    path('remove_order/', remove_order, name='remove_order'),
+    path('calculate_total_price/', calculate_total_price, name='calculate_total_price'),
+    path("shop_grid/<str:product>/", shop_grid, name="shop_grid_search"),
+    # path("add_to_cart/<slug:product_id>/", add_to_cart, name= "add_to_cart"),
+
     ]
